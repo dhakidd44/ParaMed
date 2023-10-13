42fa645b22712ff07648b0d422ca8a50618e45f9
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-12 23:17:28 -0300

    Creation d une deuxieme classe dans notre  modele pour notre programmme

--- myProject/myApp/models.py
+++ myProject/myApp/models.py
@@ -10,3 +10,13 @@ class Patient(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Prescription(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    prescription_date = models.DateField()
+    doctor_name = models.CharField(max_length=100)
+    medication = models.TextField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return f"Prescription for {self.patient}"
