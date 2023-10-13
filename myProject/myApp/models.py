a2b18b93067fe00526116aa3a8e3d4d03d1f97ee
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-12 23:15:46 -0300

    Creation de modele pour notre programmme

---
+++ myProject/myApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
