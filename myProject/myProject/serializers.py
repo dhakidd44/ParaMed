0ef4f68ec4017b6b0ee9ab6968b964f5cf23adcf
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-19 18:48:20 -0300

    Amelioration du rendu du detail

--- myProject/myProject/serializers.py
+++ myProject/myProject/serializers.py
@@ -3,27 +3,28 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from myApp.models import Patient, Prescription
 
+from rest_framework import serializers
+from myApp.models import Patient, Prescription
+
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = ('first_name', 'last_name', 'date_of_birth', 'address', 'phone_number')
 
 class PrescriptionSerializer(serializers.ModelSerializer):
-    patient = PatientSerializer()  # Utilisation du sérialiseur Patient dans le sérialiseur Prescription
+    patient = PatientSerializer()
 
     class Meta:
         model = Prescription
         fields = ('patient', 'prescription_date', 'doctor_name', 'medication', 'instructions')
-        
+
 # Filtrer les prescriptions actives en utilisant .filter()
 active_prescriptions = Prescription.objects.filter(is_active=True)
 
-# Utilisez le sérialiseur PrescriptionSerializer pour sérialiser un queryset
-prescriptions = Prescription.objects.all()  # Par exemple, vous pouvez obtenir toutes les prescriptions
-
-serializer = PrescriptionSerializer(prescriptions, many=True)  # Utilisation de many=True pour renvoyer une liste
+# Utilisez le sérialiseur PrescriptionSerializer pour sérialiser les prescriptions actives
+serializer = PrescriptionSerializer(active_prescriptions, many=True)
 
 # Récupérez le résultat sous forme de liste avec la propriété .data
 serialized_data = serializer.data
 
-# Le résultat est maintenant une liste de prescriptions avec les détails des patients inclus.
+# Le résultat est maintenant une liste de prescriptions actives avec les détails des patients inclus.
